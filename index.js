6427f4a25f3c9f7d2c84ccfcfaf35ad5e2741f16
whitershade <whitershade34@gmail.com>
2017-12-17 01:22:53 +0200

    added mongose and connect remote database

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 const app = express();
 
 require('./services/passport');
 require('./routes/auth')(app);
 
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(keys.mongoURL);
 app.listen(PORT);
