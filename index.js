41ed990bce39671b41c024b6d05ff33dc3934bc7
whitershade <whitershade34@gmail.com>
2017-12-17 01:46:13 +0200

    added possibility to save user into mongoDB

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const app = express();
 
+require('./models/User'); // should be before ./services/passport because passport.js uses models
 require('./services/passport');
 require('./routes/auth')(app);
-require('./models/User');
 
 const PORT = process.env.PORT || 5000;
 
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
+const mongoose = require('mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
@@ -10,10 +13,7 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken: ' + accessToken);
-      console.log('refreshToken: ' + refreshToken);
-      console.log('profile: ' + profile);
-      console.log('done: ' + done);
+      new User({ googleId: profile.id }).save();
     }
   )
 );
