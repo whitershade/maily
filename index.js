2df145aae64b9a40753d04e885cc49da4d3ccdd8
whitershade <whitershade34@gmail.com>
2017-12-17 02:32:18 +0200

    enabled cookie sessions with passport.js

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
 const keys = require('./config/keys');
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 require('./models/User'); // should be before ./services/passport because passport.js uses models
 require('./services/passport');
 require('./routes/auth')(app);
 
-const PORT = process.env.PORT || 5000;
-
 mongoose.connect(keys.mongoURL);
+
+app.use(
+  cookieSession({
+    maxAge: 2592000000, // 30 days in milliseconds
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.listen(PORT);
