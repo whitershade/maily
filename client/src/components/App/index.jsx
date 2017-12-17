ace28fa8ad9453440b46475d7862152b10e572e5
whitershade <whitershade34@gmail.com>
2017-12-18 01:47:28 +0200

    added initial routing to react client

--- client/src/components/App/index.jsx
+++ client/src/components/App/index.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-const App = () => <div>Hi there!</div>;
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+const NoPage = () => <h2>404</h2>;
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/surveys/new" component={SurveyNew} />
+          <Route path="/surveys" component={Dashboard} />
+          <Route exact path="/" component={Landing} />
+          <Route component={NoPage} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
