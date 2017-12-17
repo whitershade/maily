61584294941f34d8da8b45b4765144e0cfe9fc05
whitershade <whitershade34@gmail.com>
2017-12-17 02:55:12 +0200

    added possibility to login

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const passport = require('passport');
 const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-require('./models/User'); // should be before ./services/passport because passport.js uses models
-require('./services/passport');
-require('./routes/auth')(app);
 
 mongoose.connect(keys.mongoURL);
 
+const app = express();
+const PORT = process.env.PORT || 5000;
 app.use(
   cookieSession({
     maxAge: 2592000000, // 30 days in milliseconds
@@ -21,4 +16,8 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
+require('./models/User'); // should be before ./services/passport because passport.js uses models
+require('./services/passport');
+require('./routes/auth')(app); // should be after all app.use
+
 app.listen(PORT);
